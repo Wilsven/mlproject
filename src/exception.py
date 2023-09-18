385240060cde7d599cdc092cf56ab33ba5e3975e
Wilsven <wilsven_leong96@hotmail.co.uk>
2023-09-18 22:00:19 +0800

    EDA and Model Training

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def get_custom_error_message(error_message):
+    _, _, exc_traceback = sys.exc_info()
+    file_name = exc_traceback.tb_frame.f_code.co_filename
+    custom_error_message = f"""
+    Error message: {error_message}; Occured in script {file_name} on line number {exc_traceback.tb_lineno}.
+    """
+
+    return custom_error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message):
+        super().__init__()
+        self.custom_error_message = get_custom_error_message(error_message)
+
+    def __str__(self):
+        return self.custom_error_message
